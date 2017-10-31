0a25e1cb41484d78e7f1fec25f40bc2d676c89b5
streiffus <tamar.curry@gmail.com>
2017-10-30 22:07:30 -0700

    Console
    Some simple commands when interfacing with a console/terminal

---
+++ libs/utils/Console.hx
@@ -0,0 +1,32 @@
+package libs.utils;
+import haxe.io.Input;
+
+/**
+ * ...
+ * @author Tamar Curry
+ */
+class Console
+{
+	}
+	
+	public static function clear():Void {
+		Sys.command("cls");
+	}
+	
+	public static function print(message:String):Void {
+		Sys.print(message);
+	}
+	
+	public static function printLn(message:String):Void {
+		Sys.println(message);
+	}
+	
+	public static function getInput(message:String=null):String {
+		var input:Input = Sys.stdin();
+		if ( message != null )
+		{
+			Sys.print(message);
+		}
+		return input.readLine();
+	}
+}
